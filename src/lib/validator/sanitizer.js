29bb5b7cb26e2ec52721094a5491f37a282d93ef
Andrea-Canale <andrea.canale@istitutoagnelli.it>
2022-12-08 10:29:30 +0100

    sanitizer: switch to arrow function

--- src/lib/validator/sanitizer.js
+++ src/lib/validator/sanitizer.js
@@ -1,7 +1,7 @@
 /**
  * @param {string} input_from_client
  */
-export function is_an_html_element(input_from_client){
-    const anti_html_regex = new RegExp("<[^>]+>");  // This catch HTML elements
-    return anti_html_regex.test(input_from_client);
-}
+
+const anti_html_regex = new RegExp("<[^>]+>"); 
+
+export const is_an_html_element = (/** @type {string} */ input_from_client) => anti_html_regex.test(input_from_client);
