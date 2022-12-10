9ae0c0d289d4572e0b6e2ae7c8152abc637c4efd
Andrea-Canale <andrea.canale@istitutoagnelli.it>
2022-12-10 10:34:44 +0100

    svelte.config.js: update csp policy for google

--- svelte.config.js
+++ svelte.config.js
@@ -9,18 +9,18 @@ const config = {
 				'frame-ancestors': ['none'],	// Non usiamo iframe nel sito per il momento
 				'base-uri': ['self'],
 				'connect-src': ['self', 'localhost'],	// Quando sapremo il dominio potrebbe essere utile cambiarlo
-				'default-src': ['self'],
+				'default-src': ['self', 'https://accounts.google.com/'],
 				'font-src': ['self'],
 				'form-action': ['self'],
-				'frame-src': ['none'], // Non usiamo iframe nel sito per il momento
+				'frame-src': ['https://accounts.google.com/'], // Non usiamo iframe nel sito per il momento
 				'img-src': ['self', 'data:'],
 				'manifest-src': ['none'],	// Non usiamo manifest nel sito per il momento, probabilmente in futuro
 				'media-src': ['none'],    // Non usiamo media(audio e video) nel sito per il momento
                 'object-src': ['none'],    // Non usiamo object, applet o embed nel sito per il momento
 				'script-src': ['self', 'unsafe-inline'],// Dovremmo rimuovere gli script inline
 				'script-src-elem': ['self', 'unsafe-inline'],
-				'style-src': ['self', 'unsafe-inline'],	// Dovremmo rimuovere gli stili inline
-				'style-src-elem': ['self', 'unsafe-inline'],
+				'style-src': ['self', 'unsafe-inline', 'https://accounts.google.com/'],	// Dovremmo rimuovere gli stili inline
+				'style-src-elem': ['self', 'unsafe-inline', 'https://accounts.google.com/'],
 				'worker-src': ['none']	// Non usiamo service-worker per il momento, probabilmente in futuro
 			}
 		},
