f67b07b978e713001464b44835eeb7e6f1939fe1
Prof. Mancuso Antonio <111191978+profmancusoa@users.noreply.github.com>
2022-12-25 20:01:37 +0100

    - fix issue-99 e issue-100 (#101)
    - aggiornata la sezione FAQ
    - aggiunta la possibilità di scrivere le FAQ in markdown

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -2,14 +2,19 @@
 	import { page_pre_title, page_title, page_action_title } from '../../js/store';
 	import Faq from '$lib/components/faq/faq.svelte';
 	import FaqSection from '$lib/components/faq/faq_section.svelte';
-    import { Logger } from '../../js/logger';
+	import { Logger } from '../../js/logger';
+	export let data;
 
-    let logger = new Logger("client");
+	let logger = new Logger('client');
 
 	//configura la pagina pre-titolo, titolo e nome del modale
 	$page_pre_title = 'Documentation';
 	$page_title = 'Frequently Asked Questions';
 	$page_action_title = '';
+
+	function faq(name) {
+		return data.faq[name];
+	}
 </script>
 
 <div class="page-wrapper">
@@ -18,66 +23,53 @@
 			<div class="card card-lg">
 				<div class="card-body">
 					<div class="space-y-4">
+						<FaqSection title="1. Utenti" number="1">
+							<Faq
+								show={true}
+								number="1-1"
+								parent_number="1"
+								question="Come posso segnalare un malfunzionamento, un suggerimento o una richiesta di modifica?"
+								answer={faq('faq1_1')}
+							/>
+							<Faq
+								show={false}
+								number="1-2"
+								parent_number="1"
+								question="Posso effettuare il login con la mia email privata?"
+								answer={faq('faq1_2')}
+							/>
+						</FaqSection>
+						<FaqSection title="2. PCTO" number="2">
+							<Faq
+								show={true}
+								number="2-1"
+								parent_number="2"
+								question="Domanda #1?"
+								answer="<p>Risposta #1</p>"
+							/>
+							<Faq
+								show={false}
+								number="2-2"
+								parent_number="2"
+								question="Domanda #2?"
+								answer="<p>Risposta #2</p>"
+							/>
+						</FaqSection>
 						<FaqSection title="0. Sviluppatori" number="0">
 							<Faq
-                                show={true}
+								show={true}
 								number="0-1"
 								parent_number="0"
-								title="Posso contribuire al progetto?"
-							>
-								<p>
-									Certamente, ogni contributo è benvenuto. Vai sul <a
-										href="https://github.com/ittagnelli/SARP"
-										target="_blank">github</a
-									> del progetto ed inizia a leggere la documentazione, ad esplorare le issue aperte
-									e richiedi la partecipazione.
-								</p>
-							</Faq>
+								tiquestiontle="Posso contribuire al progetto?"
+								answer={faq('faq0_1')}
+							/>
 							<Faq
 								show={false}
 								number="0-2"
 								parent_number="0"
-								title="Dove trovo informazioni utili allo sviluppo?"
-							>
-								<p>
-									Sul repository github del progetto è presente un <a
-										href="https://github.com/ittagnelli/SARP/wiki"
-										target="_blank">Wiki</a
-									>
-									che fornisce tutte le informazioni necessarie ad iniziare lo sviluppo e contribuire
-									al progetto
-								</p>
-							</Faq>
-						</FaqSection>
-						<FaqSection title="1. Utenti" number="1">
-							<Faq show={true} number="1-1" parent_number="1" title="Domanda #1?">
-								<p>Risposta #1</p>
-							</Faq>
-							<Faq show={false} number="1-2" parent_number="1" title="Domanda #2?">
-								<p>
-                                    Risposta #2
-                                </p>
-							</Faq>
-						</FaqSection>
-                        <FaqSection title="2. PCTO" number="2">
-							<Faq show={true} number="2-1" parent_number="2" title="Domanda #1?">
-								<p>Risposta #1</p>
-							</Faq>
-							<Faq show={false} number="2-2" parent_number="2" title="Domanda #2?">
-								<p>
-                                    Risposta #2
-                                </p>
-							</Faq>
-                            <Faq show={false} number="2-3" parent_number="2" title="Domanda #3?">
-								<p>
-                                    Risposta #3
-                                </p>
-							</Faq>
-                            <Faq show={false} number="2-4" parent_number="2" title="Domanda #4?">
-								<p>
-                                    Risposta #4
-                                </p>
-							</Faq>
+								question="Dove trovo informazioni utili allo sviluppo?"
+								answer={faq('faq0_2')}
+							/>
 						</FaqSection>
 					</div>
 				</div>
---
+++ static/faq/faq1_2.md
@@ -0,0 +1,5 @@
+Al momento per decisione progettuale, per poter accedere a SARP è necessario utilizzare il proprio account istituzionale.
+                                
+E' anche necessario essere abilitati all'accesso dagli amministratori di SARP
+
+Se dovesse permanere la necessità assoluta di usare un account email privato, contattare il team di sviluppo di SARP e riportare la richiesta tramite la pagina delle segnalazioni
