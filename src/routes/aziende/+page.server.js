d538fc6a9e0efac5c6c6d1b12c1407d78e21d8a1
profmancusoa <profmancusoa@gmail.com>
2022-12-28 19:21:35 +0100

    - spostato i file rilevanti da static a server_static - aggiunto gestione eccezione in caso il file non possa essere generato - aggiunti diretory e nome template in .env

--- src/routes/aziende/+page.server.js
+++ src/routes/aziende/+page.server.js
@@ -4,46 +4,46 @@ import Docxtemplater from 'docxtemplater';
 import fs from 'fs';
 import path from 'path';
 import { redirect } from '@sveltejs/kit';
-import { route_protect, multi_user_where, user_id } from '../../js/helper';
+import { route_protect, multi_user_where, user_id, raise_error } from '../../js/helper';
 import { Logger } from '../../js/logger';
+import { PUBLIC_PCTO_TEMPLATES_DIR, PUBLIC_PCTO_TEMPLATE_AZIENDE } from '$env/static/public';
 
-let logger = new Logger("seerver"); //instanzia il logger
+let logger = new Logger('seerver'); //instanzia il logger
 const SARP = new PrismaDB(); //Istanzia il client SARP DB
 
-
 export async function load({ locals }) {
-    route_protect(locals);
+	route_protect(locals);
 
-    // query SQL al DB per tutte le entry nella tabella todo
+	// query SQL al DB per tutte le entry nella tabella todo
 	const companies = await SARP.pcto_Azienda.findMany({
 		orderBy: [{ id: 'desc' }],
-        where: multi_user_where(locals) 
+		where: multi_user_where(locals)
 	});
 
 	// restituisco il risultato della query SQL
-    return {aziende: companies};
+	return { aziende: companies };
 }
 
 export const actions = {
 	create: async ({ cookies, request, locals }) => {
 		const form_data = await request.formData();
 
-        SARP.set_session(locals); // passa la sessione all'audit
+		SARP.set_session(locals); // passa la sessione all'audit
 		await SARP.pcto_Azienda.create({
 			data: {
-                creatoDa: user_id(locals),
-                idConvenzione: form_data.get('idConvenzione'),
-                nome: form_data.get('nome'),
-                indirizzo: form_data.get('indirizzo'),
-                piva: form_data.get('piva'),
-                telefono: form_data.get('telefono'),
-                direttore_nome: form_data.get('direttore_nome'),
-                direttore_natoA: form_data.get('direttore_natoA'),
-                direttore_natoIl: new Date(form_data.get('direttore_natoIl')),
-                direttore_codiceF: form_data.get('direttore_codiceF'),
-                dataConvenzione: new Date(form_data.get('dataConvenzione')),
+				creatoDa: user_id(locals),
+				idConvenzione: form_data.get('idConvenzione'),
+				nome: form_data.get('nome'),
+				indirizzo: form_data.get('indirizzo'),
+				piva: form_data.get('piva'),
+				telefono: form_data.get('telefono'),
+				direttore_nome: form_data.get('direttore_nome'),
+				direttore_natoA: form_data.get('direttore_natoA'),
+				direttore_natoIl: new Date(form_data.get('direttore_natoIl')),
+				direttore_codiceF: form_data.get('direttore_codiceF'),
+				dataConvenzione: new Date(form_data.get('dataConvenzione')),
 				dataProtocollo: new Date(form_data.get('dataProtocollo')),
-                istituto: form_data.get('istituto')
+				istituto: form_data.get('istituto')
 			}
 		});
 	},
@@ -52,22 +52,22 @@ export const actions = {
 		const form_data = await request.formData();
 		let id = form_data.get('id');
 
-        SARP.set_session(locals); // passa la sessione all'audit
+		SARP.set_session(locals); // passa la sessione all'audit
 		await SARP.pcto_Azienda.update({
 			where: { id: +id },
 			data: {
-                idConvenzione: form_data.get('idConvenzione'),
-                nome: form_data.get('nome'),
-                indirizzo: form_data.get('indirizzo'),
-                piva: form_data.get('piva'),
-                telefono: form_data.get('telefono'),
-                direttore_nome: form_data.get('direttore_nome'),
-                direttore_natoA: form_data.get('direttore_natoA'),
-                direttore_natoIl: new Date(form_data.get('direttore_natoIl')),
-                direttore_codiceF: form_data.get('direttore_codiceF'),
-                dataConvenzione: new Date(form_data.get('dataConvenzione')),
+				idConvenzione: form_data.get('idConvenzione'),
+				nome: form_data.get('nome'),
+				indirizzo: form_data.get('indirizzo'),
+				piva: form_data.get('piva'),
+				telefono: form_data.get('telefono'),
+				direttore_nome: form_data.get('direttore_nome'),
+				direttore_natoA: form_data.get('direttore_natoA'),
+				direttore_natoIl: new Date(form_data.get('direttore_natoIl')),
+				direttore_codiceF: form_data.get('direttore_codiceF'),
+				dataConvenzione: new Date(form_data.get('dataConvenzione')),
 				dataProtocollo: new Date(form_data.get('dataProtocollo')),
-                istituto: form_data.get('istituto'),
+				istituto: form_data.get('istituto')
 			}
 		});
 	},
@@ -76,41 +76,51 @@ export const actions = {
 		const form_data = await request.formData();
 		const id = form_data.get('id');
 
-        SARP.set_session(locals); // passa la sessione all'audit
+		SARP.set_session(locals); // passa la sessione all'audit
 		await SARP.pcto_Azienda.delete({
 			where: { id: +id }
 		});
 	},
 
-    pdf: async ({ cookies, request }) => {
-        const form_data = await request.formData();
-        const id = form_data.get('id');
-        
-        // preleva l'azienda dal DB
-        const company = await SARP.pcto_Azienda.findUnique({
-            where: { id: +id }
-        });
-        //arricchisce l'oggetto
-        company['today'] =  new Date().toLocaleDateString();
-        company['direttore_natoIl'] = company['direttore_natoIl'].toLocaleDateString();
+	pdf: async ({ cookies, request }) => {
+		let buf, company;
+		try {
+			const form_data = await request.formData();
+			const id = form_data.get('id');
 
-        const content = fs.readFileSync(
-            path.resolve("static/pcto_templates/", "01-Convenzione-generale.docx"), "binary");
-        
-        const zip = new PizZip(content);
-        
-        const doc = new Docxtemplater(zip, {
-            paragraphLoop: true,
-            linebreaks: true,
-        });
-        
-        doc.render(company);
-        
-        const buf = doc.getZip().generate({
-            type: "nodebuffer",
-            compression: "DEFLATE",
-        });
-        fs.writeFileSync(path.resolve("static/pcto_output/", `01-Convenzione-generale-${company.idConvenzione}.docx`), buf);
-        throw redirect(303, `pcto_output/01-Convenzione-generale-${company.idConvenzione}.docx`);   
-    }   
+			// preleva l'azienda dal DB
+			company = await SARP.pcto_Azienda.findUnique({
+				where: { id: +id }
+			});
+			//arricchisce l'oggetto
+			company['today'] = new Date().toLocaleDateString();
+			company['direttore_natoIl'] = company['direttore_natoIl'].toLocaleDateString();
+
+			const content = fs.readFileSync(
+				path.resolve(PUBLIC_PCTO_TEMPLATES_DIR, PUBLIC_PCTO_TEMPLATE_AZIENDE),
+				'binary'
+			);
+
+			const zip = new PizZip(content);
+
+			const doc = new Docxtemplater(zip, {
+				paragraphLoop: true,
+				linebreaks: true
+			});
+
+			doc.render(company);
+
+			buf = doc.getZip().generate({
+				type: 'nodebuffer',
+				compression: 'DEFLATE'
+			});
+		} catch (exception) {
+			raise_error(500, 101, 'Impossibile generare il file a partire dal template');
+		}
+		fs.writeFileSync(
+			path.resolve('static/pcto_output/', `01-Convenzione-generale-${company.idConvenzione}.docx`),
+			buf
+		);
+		throw redirect(303, `pcto_output/01-Convenzione-generale-${company.idConvenzione}.docx`);
+	}
 };
