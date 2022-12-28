80a17de14bd2032c6a78d57db5149237bd1166b9
Andrea-Canale <andrea.canale@istitutoagnelli.it>
2022-12-28 19:34:06 +0100

    presenze: aggiunta gestione dell'eccezioni

--- src/routes/presenze/+page.server.js
+++ src/routes/presenze/+page.server.js
@@ -1,36 +1,47 @@
 import { PrismaDB } from '../../js/prisma_db';
-import { route_protect } from '../../js/helper';
+import { raise_error, route_protect } from '../../js/helper';
 import { Logger } from '../../js/logger';
 
 let logger = new Logger("server"); //instanzia il logger
 const SARP = new PrismaDB(); //Istanzia il client SARP DB
 
+// @ts-ignore
+function catch_error(exception, type) {
+    logger.error(JSON.stringify(exception)); //PROF: error è un oggetto ma serve qualcosa di più complicato. per il momento lascialo così. ho gia risolto in hooks nella versione 9.0
+    raise_error(500, 100, `Errore irreversibile durante ${type} della presenza. TIMESTAMP: ${new Date().toISOString()} Riportare questo messaggio agli sviluppatori`);    // TIMESTAMP ci serve per capire l'errore all'interno del log
+}
+
 export async function load({ locals }) {
     route_protect(locals);
+	try {
+		// query SQL al DB per tutte le entry nella tabella todo
+		const presenze = await SARP.pcto_Presenza.findMany({
+			orderBy: [{ id: 'desc' }],
+			include: {
+				presenza: true,
+				lavoraPer: true
+			}
+		});
+	
+		const stages = await SARP.pcto_Pcto.findMany({
+			orderBy: [{ id: 'desc' }],
+			include: {
+				offertoDa: true,
+				svoltoDa: true
+	
+			}
+		});
+	
+		// restituisco il risultato della query SQL
+		return {
+			presenze: presenze,
+			stages: stages
+		}	
+	} catch (error) {
+		logger.error(JSON.stringify(exception)); //PROF: error è un oggetto ma serve qualcosa di più complicato. per il momento lascialo così. ho gia risolto in hooks nella versione 9.0
+		raise_error(500, 100, `Errore durante la ricerca delle presenze. TIMESTAMP: ${new Date().toISOString()} Riportare questo messaggio agli sviluppatori`);    // TIMESTAMP ci serve per capire l'errore all'interno del log
+	}
 
-	// query SQL al DB per tutte le entry nella tabella todo
-	const presenze = await SARP.pcto_Presenza.findMany({
-		orderBy: [{ id: 'desc' }],
-        include: {
-			presenza: true,
-            lavoraPer: true
-		}
-	});
-
-    const stages = await SARP.pcto_Pcto.findMany({
-		orderBy: [{ id: 'desc' }],
-        include: {
-			offertoDa: true,
-            svoltoDa: true
-
-		}
-	});
-
-	// restituisco il risultato della query SQL
-	return {
-        presenze: presenze,
-        stages: stages
-    }
 }
 
 export const actions = {
@@ -42,15 +53,20 @@ export const actions = {
         let mm_fine = form_data.get('oraFine').split(':')[1];
         
         SARP.set_session(locals); // passa la sessione all'audit
-		await SARP.pcto_Presenza.create({
-			data: {
-                dataPresenza: new Date(form_data.get('dataPresenza')),
-                oraInizio: new Date(1970, 1, 1, hh_inizio, mm_inizio),
-                oraFine: new Date(1970,1 ,1, hh_fine, mm_fine),
-                idUtente: +form_data.get('studente'),
-                idPcto: +form_data.get('stage')
-			}
-		});
+		try {
+			await SARP.pcto_Presenza.create({
+				data: {
+					dataPresenza: new Date(form_data.get('dataPresenza')),
+					oraInizio: new Date(1970, 1, 1, hh_inizio, mm_inizio),
+					oraFine: new Date(1970,1 ,1, hh_fine, mm_fine),
+					idUtente: +form_data.get('studente'),
+					idPcto: +form_data.get('stage')
+				}
+			});			
+		} catch (error) {
+			catch_error(error, "l'inserimento");
+		}
+
 	},
 
 	update: async ({ cookies, request, locals }) => {
@@ -62,16 +78,20 @@ export const actions = {
         let mm_fine = form_data.get('oraFine').split(':')[1];
         
         SARP.set_session(locals); // passa la sessione all'audit
-		await SARP.pcto_Presenza.update({
-			where: { id: +id },
-			data: {
-                dataPresenza: new Date(form_data.get('dataPresenza')),
-                oraInizio: new Date(1970, 1, 1, hh_inizio, mm_inizio),
-                oraFine: new Date(1970,1 ,1, hh_fine, mm_fine),
-                idUtente: +form_data.get('studente'),
-                idPcto: +form_data.get('stage')
-			}
-		});
+		try {
+			await SARP.pcto_Presenza.update({
+				where: { id: +id },
+				data: {
+					dataPresenza: new Date(form_data.get('dataPresenza')),
+					oraInizio: new Date(1970, 1, 1, hh_inizio, mm_inizio),
+					oraFine: new Date(1970,1 ,1, hh_fine, mm_fine),
+					idUtente: +form_data.get('studente'),
+					idPcto: +form_data.get('stage')
+				}
+			});	
+		} catch (error) {
+			catch_error(error, "l'aggiornamento")
+		}
 	},
 
 	delete: async ({ cookies, request, locals }) => {
@@ -79,8 +99,12 @@ export const actions = {
 		const id = form_data.get('id');
 
         SARP.set_session(locals); // passa la sessione all'audit
-		await SARP.pcto_Presenza.delete({
-			where: { id: +id }
-		});
+		try {
+			await SARP.pcto_Presenza.delete({
+				where: { id: +id }
+			});
+		} catch (error) {
+			catch_error(error, "l'eliminazione");
+		}
 	}
 };
