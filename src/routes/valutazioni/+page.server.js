ce005e9e74a2e55f2ca735cb65784f3fdfc39cad
Andrea-Canale <andrea.canale@istitutoagnelli.it>
2022-12-28 19:40:53 +0100

    valutazioni: aggiunta gestione delle eccezioni

--- src/routes/valutazioni/+page.server.js
+++ src/routes/valutazioni/+page.server.js
@@ -1,23 +1,34 @@
 import { PrismaDB } from '../../js/prisma_db';
-import { route_protect } from '../../js/helper';
+import { raise_error, route_protect } from '../../js/helper';
 import { Logger } from '../../js/logger';
 
 let logger = new Logger("server"); //instanzia il logger
 const SARP = new PrismaDB(); //Istanzia il client SARP DB
 
+// @ts-ignore
+function catch_error(exception, type) {
+    logger.error(JSON.stringify(exception)); //PROF: error è un oggetto ma serve qualcosa di più complicato. per il momento lascialo così. ho gia risolto in hooks nella versione 9.0
+    raise_error(500, 100, `Errore irreversibile durante ${type} della valutazione. TIMESTAMP: ${new Date().toISOString()} Riportare questo messaggio agli sviluppatori`);    // TIMESTAMP ci serve per capire l'errore all'interno del log
+}
+
 export async function load({ locals }) {
     route_protect(locals);
+    try {
+        // query SQL al DB per tutte le entry nella tabella valutazioni
+        const evaluations = await SARP.pcto_Valutazione.findMany({
+            include: {
+                utente: true,
+                pcto: true
+            }
+        });
+        const pcto = await SARP.pcto_Pcto.findMany();
 
-	// query SQL al DB per tutte le entry nella tabella valutazioni
-	const evaluations = await SARP.pcto_Valutazione.findMany({
-		include: {
-			utente: true,
-			pcto: true
-		}
-	});
-    const pcto = await SARP.pcto_Pcto.findMany();
+        return { valutazioni: evaluations, stages: pcto };       
+    } catch (error) {
+        logger.error(JSON.stringify(exception)); //PROF: error è un oggetto ma serve qualcosa di più complicato. per il momento lascialo così. ho gia risolto in hooks nella versione 9.0
+		raise_error(500, 100, `Errore durante la ricerca delle valutazioni. TIMESTAMP: ${new Date().toISOString()} Riportare questo messaggio agli sviluppatori`);    // TIMESTAMP ci serve per capire l'errore all'interno del log
+    }
 
-    return { valutazioni: evaluations, stages: pcto };
 }
 
 export const actions = {
@@ -27,13 +38,18 @@ export const actions = {
         const qna = form_data.get('qna');
 
         SARP.set_session(locals); // passa la sessione all'audit
-		await SARP.pcto_Valutazione.create({
-            data: {
-				idPcto: +id_pcto,
-                idUtente: locals.session.login.id,
-                qna: qna
-			}
-		});
+        try {
+            await SARP.pcto_Valutazione.create({
+                data: {
+                    idPcto: +id_pcto,
+                    idUtente: locals.session.login.id,
+                    qna: qna
+                }
+            });
+        } catch (error) {
+            catch_error(error, "l'inserimento");
+        }
+
 	},
 
 	update: async ({ cookies, request, locals }) => {
@@ -43,14 +59,19 @@ export const actions = {
         const qna = form_data.get('qna');
 
         SARP.set_session(locals); // passa la sessione all'audit
-		await SARP.pcto_Valutazione.update({
-            where: { id: +id },
-            data: {
-                idPcto: +id_pcto,
-                idUtente: locals.session.login.id,	// Default user, we need to change this
-                qna: qna
-			}
-		});
+        try {
+            await SARP.pcto_Valutazione.update({
+                where: { id: +id },
+                data: {
+                    idPcto: +id_pcto,
+                    idUtente: locals.session.login.id,	// Default user, we need to change this
+                    qna: qna
+                }
+            });          
+        } catch (error) {
+            catch_error(error, "la modifica");
+        }
+
 	},
 
 	delete: async ({ cookies, request, locals }) => {
@@ -58,9 +79,13 @@ export const actions = {
         const id = form_data.get('id');
 
         SARP.set_session(locals); // passa la sessione all'audit
-        await SARP.pcto_Valutazione.delete({
-            where: { id: +id }
-		});
+        try {
+            await SARP.pcto_Valutazione.delete({
+                where: { id: +id }
+            });  
+        } catch (error) {
+            catch_error(error, "l'eliminazione");
+        }
 		
 	}
 };
