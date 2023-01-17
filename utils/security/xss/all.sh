50b3e590aa183cdd54cdfde032d9f8ca31536a27
Andrea-Canale <andrea.canale@istitutoagnelli.it>
2023-01-17 12:47:03 +0100

    security: add some endpoints in script

--- utils/security/xss/all.sh
+++ utils/security/xss/all.sh
@@ -1,1 +1,4 @@
-python3 xss.py -u http://localhost:5173/logger -c 86575ef6-9d6b-4649-875f-ce65448e56d4 -p payloads/logger.json
+node xss.js http://localhost:5173/logger $1 payloads/logger.json
+node xss.js http://localhost:5173/aziende?/create $1 payloads/aziende.json 
+node xss.js http://localhost:5173/presenze?/create $1 payloads/presenze.json 
+node xss.js http://localhost:5173/utente?/create $1 payloads/utente.json 
