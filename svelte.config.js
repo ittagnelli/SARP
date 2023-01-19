037291db5a85aba5b8c545e011027e9ed8ac7568
Andrea-Canale <andrea.canale@istitutoagnelli.it>
2023-01-19 10:58:40 +0100

    add service-worker support

--- svelte.config.js
+++ svelte.config.js
@@ -7,23 +7,26 @@ const config = {
 		adapter: adapter({ out: 'SARP_BUILD' }),
 		csp: {
 			directives: {
-				'frame-ancestors': ['none'], // Non usiamo iframe nel sito per il momento
+				'frame-ancestors': ['none'],	// Non usiamo iframe nel sito per il momento
 				'base-uri': ['self'],
-				'connect-src': ['self', 'localhost', 'https://sarp.agnelli.it'],	// Quando sapremo il dominio potrebbe essere utile cambiarlo
+				'connect-src': ['self', 'localhost', 'https://sarp.agnelli.it'],
 				'default-src': ['self', 'https://accounts.google.com/'],
 				'font-src': ['self'],
 				'form-action': ['self'],
 				'frame-src': ['https://accounts.google.com/'], // Non usiamo iframe nel sito per il momento
 				'img-src': ['self', 'data:'],
-				'manifest-src': ['none'],	// Non usiamo manifest nel sito per il momento, probabilmente in futuro
+				'manifest-src': ['self'],
 				'media-src': ['none'],    // Non usiamo media(audio e video) nel sito per il momento
                 'object-src': ['none'],    // Non usiamo object, applet o embed nel sito per il momento
-				'script-src': ['self'],// Dovremmo rimuovere gli script inline
-				'script-src-elem': ['self'],
-				'style-src': ['self', 'https://accounts.google.com/'],	// Dovremmo rimuovere gli stili inline
-				'style-src-elem': ['self','https://accounts.google.com/'],
-				'worker-src': ['none']	// Non usiamo service-worker per il momento, probabilmente in futuro
+				'script-src': ['self', "unsafe-inline"],// Dovremmo rimuovere gli script inline
+				'script-src-elem': ['self', "unsafe-inline"],
+				'style-src': ['self', 'https://accounts.google.com/', "unsafe-inline"],	// Dovremmo rimuovere gli stili inline
+				'style-src-elem': ['self','https://accounts.google.com/', "unsafe-inline"],
+				'worker-src': ['self']
 			}
+		},
+		version: {
+			name: "0.11.0"
 		},
 		csrf: {
 			checkOrigin: true
