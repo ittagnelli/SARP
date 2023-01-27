2c5210d559f0db8dd082f31d868549ae98a07b5d
Andrea-Canale <andrea.canale@istitutoagnelli.it>
2023-01-27 15:37:33 +0100

    convert_to_pdf: fix word converting and add padding to document

--- src/routes/convert-to-print/+page.server.js
+++ src/routes/convert-to-print/+page.server.js
@@ -1,9 +1,11 @@
 import { raise_error, route_protect } from '../../js/helper'; //PROF: usa l'helper raise_erorr che ho cretao qualche settimana fa
 import { Logger } from '../../js/logger';
-import { convert} from 'node-unoconv';
 import fs from 'fs';
 import PDFMerger from 'pdf-merger-js';
 import crypto from 'crypto';
+import { exec } from 'child_process';
+import { PUBLIC_PDF_TMP_FILE, PUBLIC_PDF_BLANK_FILE } from '$env/static/public';
+import pdf_counter from "pdf-page-counter";
 
 let logger = new Logger('server'); //instanzia il logger
 
@@ -13,14 +15,6 @@ function cleanup(files){
     });
 }
 
-async function docx2pdf(file_name){
-    await convert(`tmp/${file_name}`, {
-        output: file_name.split(".")[0] + ".pdf"
-    });
-    fs.rmSync(`temp/${file_name}`); // Rimuovo il file docx, non serve più
-    return file_name.split(".")[0] + ".pdf";   // Cambiamo il nome del file da docx -> pdf
-}
-
 const random_name = () => crypto.randomBytes(20).toString('hex');   // Genera 20 bytes casuali e li converte in esadecimale
 
 const merger = new PDFMerger();
@@ -28,7 +22,7 @@ const merger = new PDFMerger();
 export const actions = {
     pdf: async ({ request, locals }) => {
         route_protect(locals);  // Controlla che l'utente sia autenticato
-
+        
         const form_data = await request.formData();
 
         const files = form_data.getAll('file-to-convert');
@@ -39,16 +33,24 @@ export const actions = {
             for (const file of files) {
 
                 let file_name = file.name;
+                file_name.replace(" ", "");
 
                 const extension = file_name.split(".")[1];  // L'estensione del file test.pdf sarà [test, pdf]
 
                 fs.writeFileSync(`tmp/${file_name}`, Buffer.from(await file.arrayBuffer()));    // Scrivo il file nella cartella temporanea
 
                 if(extension == "docx" || extension == "doc"){  // Se il file è Word dobbiamo convertirlo in PDF
-                    file_name = await docx2pdf(file);
+                    const cmd = `libreoffice --headless --convert-to pdf --outdir ${PUBLIC_PDF_TMP_FILE} ${PUBLIC_PDF_TMP_FILE}/${file_name}`;
+                    exec(cmd);
+                    file_name = file_name.split(".")[0] + ".pdf";
                 }
 
+                const our_pdf = fs.readFileSync(`tmp/${file_name}`);
+                const pages = await pdf_counter(our_pdf);
                 await merger.add(`tmp/${file_name}`);   // Aggiungo il pd// Usiamo un for classico al posto di forEach per il corretto merge dei filef al nuovo file
+                if(pages.numpages % 2 != 0){    // Aggingiamo una pagina se la verifica è dispari
+                    await merger.add(PUBLIC_PDF_BLANK_FILE);
+                }
             }
             const file_name_of_merged_pdf = random_name() + ".pdf"; // Generiamo un nome casuale per evitare sovrascritture
             
@@ -60,6 +62,7 @@ export const actions = {
 			};
 
         } catch (exception) {
+            console.log(exception);
             logger.error(JSON.stringify(exception)); 
             raise_error(500, 100, `Errore irreversibile durante la generazione del PDF. TIMESTAMP: ${new Date().toISOString()} Riportare questo messaggio agli sviluppatori`); 
         }
