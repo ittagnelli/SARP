fac6401c60dda56e9a5146178b9e46f9a6c16072
Prof. Mancuso Antonio <111191978+profmancusoa@users.noreply.github.com>
2023-08-04 01:22:08 +0200

    - fix#337: Alert Messages (#345)
    - Aggiunto componente MessageBox per visualizzare messaggi di tipo success, warning e danger
    - Il messaggio ha un titolo ed un testo
    - Il messaggio scompare automaticamente con un delay configurabile

--- src/js/store.js
+++ src/js/store.js
@@ -4,3 +4,11 @@ export const page_pre_title = writable('PRE-TITLE'); // page pre-title
 export const page_title = writable('TITLE'); // page title
 export const page_action_title = writable(); // page action title
 export const page_action_modal = writable('id'); // page action modal id
+
+// store variable for MessageBox component
+export let mb_type = writable();
+export let mb_color = writable();
+export let mb_title = writable();
+export let mb_message = writable();
+export let mb_show = writable(false);
+
