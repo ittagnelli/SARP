3b89636a593fa01189fa0486039e8a4ab554689c
profmancusoa <profmancusoa@gmail.com>
2023-09-21 11:39:54 +0200

    quick fix on libro regex

--- src/routes/programmazione/template/+page.svelte
+++ src/routes/programmazione/template/+page.svelte
@@ -71,7 +71,7 @@
                         //test against this format: 
                         // Cognome N.,Titolo,Casa Editrice, Anno Edizione (es: Boscaini M.,Imparare a programmare,Apogeo,2023)
                         // return /^[A-Z][A-Z a-z]* [A-Z].,[A-Z][A-Za-z0-9 -.]*,[A-Z][A-Za-z0-9 -.]*,[1-2][0-9][0-9][0-9]$/.test(value);
-                        return /^([A-Z][A-Za-z ]+ [A-Z]\.,){1,3}[A-Z][A-Za-z -]+,[A-Z][A-Za-z -]+,[1-2][0-9]{3}$/.test(value);
+                        return /^([A-Z][A-Za-z ]+ [A-Z]\.,){1,3}[A-Z][0-9A-Za-z -]+,[A-Z][0-9A-Za-z -]+,[1-2][0-9]{3}$/.test(value);
                     }
                 })
             ),
