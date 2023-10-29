5ea2901f6f284a26a5a8f4c66dcfbf247988a245
Prof. Mancuso Antonio <111191978+profmancusoa@users.noreply.github.com>
2023-10-29 15:54:19 +0100

    - `fix#434`: Documento programmazione classe senza programmi (#435)
    - Ordinate le materie nei documenti di programmazione in ordine alfabetico

--- src/js/helper.js
+++ src/js/helper.js
@@ -380,3 +380,9 @@ export const titlecase = (string) => {
            }).
            join(' ');
 }
+
+export const sort_strings = (s1, s2) => {
+    if(s1 > s2) return 1;
+    if(s1 < s2) return -1;
+    return 0;
+}
