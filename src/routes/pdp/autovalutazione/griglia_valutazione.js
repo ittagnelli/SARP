f499697e98833fc994f7a760b39f6aa96d4a0396
profmancusoa <profmancusoa@gmail.com>
2024-03-21 11:16:27 +0100

    - fix#499: Refactoring PDP

---
+++ src/routes/pdp/autovalutazione/griglia_valutazione.js
@@ -0,0 +1,419 @@
+let _griglia_valutazione = [
+    {
+        qid: 1,
+        question: 'Manifesta difficoltà di lettura/scrittura',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 2,
+        question: 'Manifesta difficoltà di comprensione del testo',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 3,
+        question: 'Manifesta difficoltà di espressione orale',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 4,
+        question: 'Manifesta difficoltà logico/matematiche',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 5,
+        question: 'Manifesta difficoltà nel rispetto delle regole',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 6,
+        question: 'Manifesta difficoltà nel mantenere l’attenzione durante le spiegazioni',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 7,
+        question: 'Svolge regolarmente i compiti a casa',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 8,
+        question: 'Consegna gli schemi all\'insegnante secondo le modalità concordate',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 9,
+        question: 'Esegue le consegne che gli vengono proposte in classe',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 10,
+        question: 'Manifesta difficoltà nella comprensione delle consegne proposte',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 11,
+        question: 'Disturba lo svolgimento delle lezioni (distrae i compagni, ecc.)',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 12,
+        question: 'Dà poco valore ai richiami dell’insegnante',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 13,
+        question: 'Manifesta difficoltà a stare fermo nel proprio banco',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 14,
+        question: 'Si fa distrarre dai compagni',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 15,
+        question: 'Manifesta timidezza',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 16,
+        question: 'I compagni lo escludono dalle attività scolastiche',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 17,
+        question: 'I compagni lo escludono nei momenti non strutturati',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 18,
+        question: 'Tende ad autoescludersi dalle attività scolastiche',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 19,
+        question: 'Tende ad autoescludersi nei momenti non strutturati',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 20,
+        question: 'Porta i materiali necessari per le attività scolastiche',
+        answers: [
+            { aid: 'a', answer: 'Mai' },
+            { aid: 'b', answer: 'Talvolta' },
+            { aid: 'c', answer: 'Spesso' },
+            { aid: 'd', answer: 'Sempre' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 21,
+        question: 'Partecipazione durante la lezione con domande e riflessioni',
+        answers: [
+            { aid: 'a', answer: 'Non adeguata' },
+            { aid: 'b', answer: 'Poco adeguata' },
+            { aid: 'c', answer: 'Adeguata' },
+            { aid: 'd', answer: 'Molto adeguata' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 22,
+        question: 'Consapevolezza delle proprie difficoltà',
+        answers: [
+            { aid: 'a', answer: 'Non adeguata' },
+            { aid: 'b', answer: 'Poco adeguata' },
+            { aid: 'c', answer: 'Adeguata' },
+            { aid: 'd', answer: 'Molto adeguata' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 23,
+        question: 'Consapevolezza dei propri punti di forza',
+        answers: [
+            { aid: 'a', answer: 'Non adeguata' },
+            { aid: 'b', answer: 'Poco adeguata' },
+            { aid: 'c', answer: 'Adeguata' },
+            { aid: 'd', answer: 'Molto adeguata' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 24,
+        question: 'Regolarità frequenza scolastica',
+        answers: [
+            { aid: 'a', answer: 'Non adeguata' },
+            { aid: 'b', answer: 'Poco adeguata' },
+            { aid: 'c', answer: 'Adeguata' },
+            { aid: 'd', answer: 'Molto adeguata' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 25,
+        question: 'Accettazione e rispetto delle regole',
+        answers: [
+            { aid: 'a', answer: 'Non adeguata' },
+            { aid: 'b', answer: 'Poco adeguata' },
+            { aid: 'c', answer: 'Adeguata' },
+            { aid: 'd', answer: 'Molto adeguata' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 26,
+        question: 'Rispetto degli impegni',
+        answers: [
+            { aid: 'a', answer: 'Non adeguata' },
+            { aid: 'b', answer: 'Poco adeguata' },
+            { aid: 'c', answer: 'Adeguata' },
+            { aid: 'd', answer: 'Molto adeguata' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 27,
+        question: 'Accettazione consapevole degli strumenti compensativi e delle misure dispensativ',
+        answers: [
+            { aid: 'a', answer: 'Non adeguata' },
+            { aid: 'b', answer: 'Poco adeguata' },
+            { aid: 'c', answer: 'Adeguata' },
+            { aid: 'd', answer: 'Molto adeguata' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 28,
+        question: 'Autonomia nel lavoro',
+        answers: [
+            { aid: 'a', answer: 'Non adeguata' },
+            { aid: 'b', answer: 'Poco adeguata' },
+            { aid: 'c', answer: 'Adeguata' },
+            { aid: 'd', answer: 'Molto adeguata' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 29,
+        question: 'Sottolinea, identifica parole chiave, …',
+        answers: [
+            { aid: 'a', answer: 'Non adeguata' },
+            { aid: 'b', answer: 'Poco adeguata' },
+            { aid: 'c', answer: 'Adeguata' },
+            { aid: 'd', answer: 'Molto adeguata' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 30,
+        question: 'Costruisce schemi, mappe o diagrammi',
+        answers: [
+            { aid: 'a', answer: 'Non adeguata' },
+            { aid: 'b', answer: 'Poco adeguata' },
+            { aid: 'c', answer: 'Adeguata' },
+            { aid: 'd', answer: 'Molto adeguata' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 31,
+        question: 'Utilizza strumenti informatici (computer, correttore ortografico, software, …)',
+        answers: [
+            { aid: 'a', answer: 'Non adeguata' },
+            { aid: 'b', answer: 'Poco adeguata' },
+            { aid: 'c', answer: 'Adeguata' },
+            { aid: 'd', answer: 'Molto adeguata' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 32,
+        question: 'Usa strategie di memorizzazione (immagini, colori, riquadrature, …)',
+        answers: [
+            { aid: 'a', answer: 'Non adeguata' },
+            { aid: 'b', answer: 'Poco adeguata' },
+            { aid: 'c', answer: 'Adeguata' },
+            { aid: 'd', answer: 'Molto adeguata' },
+        ],
+        answer: 'a'
+    },
+    {
+        qid: 33,
+        question: 'Pronuncia difficoltosa',
+        answers: [
+            { aid: 'a', answer: 'SI' },
+            { aid: 'b', answer: 'NO' },
+        ],
+        answer: 'b'
+    },
+    {
+        qid: 34,
+        question: 'Difficoltà di acquisizione degli automatismi grammaticali di base',
+        answers: [
+            { aid: 'a', answer: 'SI' },
+            { aid: 'b', answer: 'NO' },
+        ],
+        answer: 'b'
+    },
+    {
+        qid: 35,
+        question: 'Difficoltà nella scrittura',
+        answers: [
+            { aid: 'a', answer: 'SI' },
+            { aid: 'b', answer: 'NO' },
+        ],
+        answer: 'b'
+    },
+    {
+        qid: 36,
+        question: 'Difficoltà acquisizione nuovo lessico',
+        answers: [
+            { aid: 'a', answer: 'SI' },
+            { aid: 'b', answer: 'NO' },
+        ],
+        answer: 'b'
+    },
+    {
+        qid: 37,
+        question: 'Notevoli differenze tra comprensione del testo scritto e orale',
+        answers: [
+            { aid: 'a', answer: 'SI' },
+            { aid: 'b', answer: 'NO' },
+        ],
+        answer: 'b'
+    },
+    {
+        qid: 38,
+        question: 'Notevoli differenze tra produzione scritta e orale',
+        answers: [
+            { aid: 'a', answer: 'SI' },
+            { aid: 'b', answer: 'NO' },
+        ],
+        answer: 'b'
+    },
+    {
+        qid: 39,
+        question: 'Interessi nei confronti delle culture e delle civiltà straniere',
+        answers: [
+            { aid: 'a', answer: 'SI' },
+            { aid: 'b', answer: 'NO' },
+        ],
+        answer: 'b'
+    }
+];
+
+export let griglia_valutazione = JSON.stringify(_griglia_valutazione);
