671700f507055c90749ae9133ce33a30bcdbe38c
profmancusoa <profmancusoa@gmail.com>
2024-04-07 18:02:58 +0200

    - fix#523: PDP Layout Patto Educativo

---
+++ src/lib/components/modal/input_select_yn.svelte
@@ -0,0 +1,31 @@
+<script>
+    export let label;
+    export let name;
+    export let val;
+
+</script>
+
+
+<label class="form-label">{label}</label>
+<div class="form-selectgroup">
+    <label class="form-selectgroup-item">
+    <input
+        type="radio"
+        name="{name}"
+        value="SI"
+        class="form-selectgroup-input"
+        bind:group={val}
+    />
+    <span class="form-selectgroup-label">SI</span>
+    </label>
+    <label class="form-selectgroup-item">
+    <input
+        type="radio"
+        name="{name}"
+        value="NO"
+        class="form-selectgroup-input"
+        bind:group={val}
+    />
+    <span class="form-selectgroup-label">NO</span>
+    </label>
+</div>
