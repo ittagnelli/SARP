bb3461684a5f3293b5592006ce7d15aba2456ebc
Prof. Mancuso Antonio <111191978+profmancusoa@users.noreply.github.com>
2024-04-07 18:03:31 +0200

    - fix#523: PDP Layout Patto Educativo (#524)

--- src/routes/pdp/patto_educativo/+page.svelte
+++ src/routes/pdp/patto_educativo/+page.svelte
@@ -4,8 +4,9 @@
 	import * as helper from '$js/helper';
 	import { Logger } from '$js/logger';
     import { griglia_pdp_c2 } from './griglia_pdp_c2.js';
-
-	export let data; //contiene l'oggetto restituito dalla funzione load() eseguita nel back-end
+	import InputSelectYn from '$lib/components/modal/input_select_yn.svelte';
+	
+    export let data; //contiene l'oggetto restituito dalla funzione load() eseguita nel back-end
 	export let form; // Risposta del form dal server
 
 	let logger = new Logger('client');
@@ -15,7 +16,7 @@
         if(s.griglia_pdp_c2 == null)
             s.griglia_pdp_c2 = griglia_pdp_c2;
     })
-    
+
 	//configura la pagina pre-titolo, titolo e nome del modale
 	$page_pre_title = 'TUTOR';
 	$page_title = 'Patto Educativo';
@@ -49,7 +50,7 @@
 			modal_action = 'create';	// Reset model string
 			form_values = {	// Reset form values
 				student_id: 0,
-				griglia_pdp_c2: '[]',
+                griglia_pdp_c2: [],
                 completo: 'NO'
 			};
 		}
@@ -101,78 +102,191 @@
 					<h5 class="modal-title">Patto Educativo</h5>
 				</div>
 				<div class="modal-body">
-                    {#each form_values.griglia_pdp_c2 as question,idx}
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="mb-3">
-                                    <label class="form-label">{idx + 1}. {question.question} </label>
-                                    <!-- gestisce la domanda 0 che è un caso speciale -->
-                                    {#if idx == 0}
-                                        <div class="form-selectgroup">
-                                            <label class="form-selectgroup-item">
-                                                    <input
-                                                        type="radio"
-                                                        name="answer{idx}"
-                                                        value="SI"
-                                                        class="form-selectgroup-input"
-                                                        bind:group={(form_values.griglia_pdp_c2[idx]).answer}
-                                                    />
-                                                <span class="form-selectgroup-label">SI</span>
-                                            </label>
-                                            <label class="form-selectgroup-item">
-                                                    <input
-                                                        type="radio"
-                                                        name="answer{idx}"
-                                                        value="NO"
-                                                        class="form-selectgroup-input"
-                                                        bind:group={(form_values.griglia_pdp_c2[idx]).answer}
-                                                    />
-                                                <span class="form-selectgroup-label">NO</span>
-                                            </label>
-                                        </div>
-                                        <br><br>
-                                        <input type="text" name="disc_1" bind:value={(form_values.griglia_pdp_c2[idx]).disc_1} />
-                                        <input type="text" name="cadenza_1" bind:value={(form_values.griglia_pdp_c2[idx]).cadenza_1} /><br><br>
-                                        <input type="text" name="disc_2" bind:value={(form_values.griglia_pdp_c2[idx]).disc_2} />
-                                        <input type="text" name="cadenza_2" bind:value={(form_values.griglia_pdp_c2[idx]).cadenza_2} /><br><br>
-                                        <input type="text" name="disc_3" bind:value={(form_values.griglia_pdp_c2[idx]).disc_3} />
-                                        <input type="text" name="cadenza_3" bind:value={(form_values.griglia_pdp_c2[idx]).cadenza_3} /><br><br>
-                                        <input type="text" name="disc_4" bind:value={(form_values.griglia_pdp_c2[idx]).disc_4} />
-                                        <input type="text" name="cadenza_4" bind:value={(form_values.griglia_pdp_c2[idx]).cadenza_4} />
-                                    {/if}
-                                    <!-- gestisce la domanda 16 che è un caso speciale -->
-                                    {#if idx == 16 || idx == 17}
-                                        <input type="text" name="answer{idx}" size="80" bind:value={(form_values.griglia_pdp_c2[idx]).answer} />
-                                    {/if}
-                                    <!-- gestisce tutte le altre domande -->
-                                    {#if idx != 0 && idx != 16 && idx != 17}
-                                    <div class="form-selectgroup">
-                                        <label class="form-selectgroup-item">
-                                                <input
-                                                    type="radio"
-                                                    name="answer{idx}"
-                                                    value="SI"
-                                                    class="form-selectgroup-input"
-                                                    bind:group={(form_values.griglia_pdp_c2[idx]).answer}
-                                                />
-                                            <span class="form-selectgroup-label">SI</span>
-                                        </label>
-                                        <label class="form-selectgroup-item">
-                                                <input
-                                                    type="radio"
-                                                    name="answer{idx}"
-                                                    value="NO"
-                                                    class="form-selectgroup-input"
-                                                    bind:group={(form_values.griglia_pdp_c2[idx]).answer}
-                                                />
-                                            <span class="form-selectgroup-label">NO</span>
-                                        </label>
-                                    </div>
-                                    {/if}
-                                </div>
+                    {#if typeof(form_values.griglia_pdp_c2) != 'string'}
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="mb-3">       
+                                <InputSelectYn
+                                    label="1. {form_values.griglia_pdp_c2[0].question}"
+                                    name="answer1"
+                                    bind:val={(form_values.griglia_pdp_c2[0]).answer}
+                                />
+                                <br><br>
+                                <input type="text" name="disc_1" bind:value={(form_values.griglia_pdp_c2[0]).disc_1} />
+                                <input type="text" name="cadenza_1" bind:value={(form_values.griglia_pdp_c2[0]).cadenza_1} /><br><br>
+                                <input type="text" name="disc_2" bind:value={(form_values.griglia_pdp_c2[0]).disc_2} />
+                                <input type="text" name="cadenza_2" bind:value={(form_values.griglia_pdp_c2[0]).cadenza_2} /><br><br>
+                                <input type="text" name="disc_3" bind:value={(form_values.griglia_pdp_c2[0]).disc_3} />
+                                <input type="text" name="cadenza_3" bind:value={(form_values.griglia_pdp_c2[0]).cadenza_3} /><br><br>
+                                <input type="text" name="disc_4" bind:value={(form_values.griglia_pdp_c2[0]).disc_4} />
+                                <input type="text" name="cadenza_4" bind:value={(form_values.griglia_pdp_c2[0]).cadenza_4} />
                             </div>
                         </div>
-                    {/each}
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="mb-3">
+                                <InputSelectYn
+                                    label="2. {form_values.griglia_pdp_c2[1].question}"
+                                    name="answer2"
+                                    bind:val={(form_values.griglia_pdp_c2[1]).answer}
+                                />       
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="mb-3">       
+                                <InputSelectYn
+                                    label="3. {form_values.griglia_pdp_c2[2].question}"
+                                    name="answer3"
+                                    bind:val={(form_values.griglia_pdp_c2[2]).answer}
+                                />    
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="mb-3">     
+                                <InputSelectYn
+                                    label="4. {form_values.griglia_pdp_c2[3].question}"
+                                    name="answer4"
+                                    bind:val={(form_values.griglia_pdp_c2[3]).answer}
+                                />      
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="mb-3">     
+                                <InputSelectYn
+                                    label="5. {form_values.griglia_pdp_c2[4].question}"
+                                    name="answer5"
+                                    bind:val={(form_values.griglia_pdp_c2[4]).answer}
+                                />       
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="mb-3">      
+                                <InputSelectYn
+                                    label="6. {form_values.griglia_pdp_c2[5].question}"
+                                    name="answer6"
+                                    bind:val={(form_values.griglia_pdp_c2[5]).answer}
+                                />      
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="mb-3">       
+                                <InputSelectYn
+                                    label="7. {form_values.griglia_pdp_c2[6].question}"
+                                    name="answer7"
+                                    bind:val={(form_values.griglia_pdp_c2[6]).answer}
+                                />     
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <div class="mb-3">     
+                                <InputSelectYn
+                                    label="8. {form_values.griglia_pdp_c2[7].question}"
+                                    name="answer8"
+                                    bind:val={(form_values.griglia_pdp_c2[7]).answer}
+                                />       
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="mb-3">      
+                                <InputSelectYn
+                                    label="9. {form_values.griglia_pdp_c2[8].question}"
+                                    name="answer9"
+                                    bind:val={(form_values.griglia_pdp_c2[8]).answer}
+                                />      
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="mb-3">     
+                                <InputSelectYn
+                                    label="10. {form_values.griglia_pdp_c2[9].question}"
+                                    name="answer10"
+                                    bind:val={(form_values.griglia_pdp_c2[9]).answer}
+                                />       
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="mb-3">      
+                                <InputSelectYn
+                                    label="11. {form_values.griglia_pdp_c2[10].question}"
+                                    name="answer11"
+                                    bind:val={(form_values.griglia_pdp_c2[10]).answer}
+                                />      
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="mb-3">       
+                                <InputSelectYn
+                                    label="12. {form_values.griglia_pdp_c2[11].question}"
+                                    name="answer12"
+                                    bind:val={(form_values.griglia_pdp_c2[11]).answer}
+                                />     
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="mb-3">     
+                                <InputSelectYn
+                                    label="13. {form_values.griglia_pdp_c2[12].question}"
+                                    name="answer13"
+                                    bind:val={(form_values.griglia_pdp_c2[12]).answer}
+                                />       
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="mb-3">      
+                                <InputSelectYn
+                                    label="14. {form_values.griglia_pdp_c2[13].question}"
+                                    name="answer14"
+                                    bind:val={(form_values.griglia_pdp_c2[13]).answer}
+                                />      
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="mb-3">       
+                                <InputSelectYn
+                                    label="15. {form_values.griglia_pdp_c2[14].question}"
+                                    name="answer15"
+                                    bind:val={(form_values.griglia_pdp_c2[14]).answer}
+                                />     
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="mb-3">       
+                                <InputSelectYn
+                                    label="16. {form_values.griglia_pdp_c2[15].question}"
+                                    name="answer16"
+                                    bind:val={(form_values.griglia_pdp_c2[15]).answer}
+                                />     
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="mb-3">       
+                                <label class="form-label">17. {form_values.griglia_pdp_c2[16].question} </label>   
+                                <input type="text" name="answer16" size="80" bind:value={(form_values.griglia_pdp_c2[16]).answer} />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="mb-3">       
+                                <label class="form-label">18. {form_values.griglia_pdp_c2[17].question} </label>   
+                                <input type="text" name="answer17" size="80" bind:value={(form_values.griglia_pdp_c2[17]).answer} />
+                            </div>
+                        </div>
+                    </div>
+                    {/if}
                     <div class="row">
                         <div class="col-lg-4">
                             <br>
