34e05e86fa6ea8a83995b2684dd6c3236742acba
Rossi-Tommaso <tommasoemanuele.rossi@istitutoagnelli.it>
2024-05-11 23:12:53 +0200

    - risolta issue-542

---
+++ prisma/migrations/20240511141906_add_sede_stage/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Ticket" ADD COLUMN "sede_stage" TEXT;
+
+-- AlterTable
+ALTER TABLE "pcto_Pcto" ADD COLUMN "sede_stage" TEXT;
+
+-- AlterTable
+ALTER TABLE "sicurezza_Corso" ADD COLUMN "sede_stage" TEXT;
