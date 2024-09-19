147559731a6ced681c8821a510bbeef3b1157763
profmancusoa <profmancusoa@gmail.com>
2024-09-19 19:37:52 +0200

    aggiornato script per disabilitare studenti vecchie V

--- utils/scripts/disable_studenti_5.js
+++ utils/scripts/disable_studenti_5.js
@@ -3,22 +3,34 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();  // Inizializzo il client di SARP
 
 (async function () {
-    const QUINTE = [10, 13, 16, 19, 24, 29]; //dirty list of classi quinte
     try {
-        console.log("UPDATING Studenti 5 anno precedente....")
-        QUINTE.forEach(async classe => {
+        //seleziona le classi V
+        const classi = await prisma.classe.findMany({
+            select: {
+                id: true,
+                classe: true,
+                istituto: true,
+                sezione: true
+            },
+            where: {
+                classe: 'V'
+            }
+        });
+
+        for (const classe of classi) {
+            console.log(`Aggiornamento studenti as precedente della V ${classe.istituto} ${classe.sezione}`);
             await prisma.utente.updateMany({
                 where: {
-                    classeId: classe
+                    classeId: classe.id
                 },
                 data: {
                     can_login: false,
-                    classeId: 1
+                    // classeId: 1
                 }
             })
-        })
-        console.log("FATTO!!!")            
-    } catch(e) {
+        }
+        console.log("Fatto!!!")
+    } catch (e) {
         console.log(e);
     }
 })();
