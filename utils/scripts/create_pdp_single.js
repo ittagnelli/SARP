1af3499907a2e45654038885fda1d3535c1cbb66
profmancusoa <profmancusoa@gmail.com>
2025-09-28 17:25:51 +0200

    - cleanup script create_pdp

--- utils/scripts/create_pdp_single.js
+++ utils/scripts/create_pdp_single.js
@@ -1,18 +1,20 @@
+// v20250928
+
 import { PrismaClient } from '@prisma/client';
 //DEV
-import { misure_dispensative } from '../../src/routes/pdp/template/dispensative.js';
-import { misure_compensative } from '../../src/routes/pdp/template/compensative.js';
-import { misure_valutative } from '../../src/routes/pdp/template/valutative.js';
-import { strategie_classe } from '../../src/routes/pdp/template/strategie_classe.js';
-import { strategie_didattiche } from '../../src/routes/pdp/template/strategie_didattiche.js';
+// import { misure_dispensative } from '../../src/routes/pdp/template/dispensative.js';
+// import { misure_compensative } from '../../src/routes/pdp/template/compensative.js';
+// import { misure_valutative } from '../../src/routes/pdp/template/valutative.js';
+// import { strategie_classe } from '../../src/routes/pdp/template/strategie_classe.js';
+// import { strategie_didattiche } from '../../src/routes/pdp/template/strategie_didattiche.js';
 
 
 //PROD
-// import { misure_dispensative } from './dispensative.js';
-// import { misure_compensative } from './compensative.js';
-// import { misure_valutative } from './valutative.js';
-// import { strategie_classe } from './strategie_classe.js';
-// import { strategie_didattiche } from './strategie_didattiche.js';
+import { misure_dispensative } from './dispensative.js';
+import { misure_compensative } from './compensative.js';
+import { misure_valutative } from './valutative.js';
+import { strategie_classe } from './strategie_classe.js';
+import { strategie_didattiche } from './strategie_didattiche.js';
 
 
 // Istanzia il client per il SARP
@@ -47,18 +49,6 @@ async function get_insegnamenti(as, idClasse) {
 }
 
 
-
-// async function get_studenti_bes(idClasse, idStudente) {
-//     return await SARP.utente.findMany({
-//         where: {
-//             id: +idStudente,
-//             bes: true,
-//             classeId: +idClasse,
-//             can_login: true
-//         }
-//     });
-// }
-
 async function add_pdp(idDocente, idInsegnamento, idStudente, as) {
     try {
         await SARP.PDP.create({
@@ -78,40 +68,6 @@ async function add_pdp(idDocente, idInsegnamento, idStudente, as) {
         console.log(e);
     }
 }
-
-
-// async function main(argv) {
-//     if (argv.length != 5) {
-//         console.log("Usage: node create_pdp_entries.js <as> <id studente> <id classe>");
-//         return 255;
-//     }
-
-//     let as = argv[2];
-//     let idStudente = argv[3];
-//     let idClasse = argv[4];
-
-//     console.log("AS:", as)
-//     console.log("STUDENTE:", idStudente);
-
-
-//     //prelevo gli insegmaneti dell'anno as
-//     let insegnamenti = await get_insegnamenti(+as, idClasse);
-
-//     //per ogni insegnamento, determino la lista degli studenti
-//     //e per ogni studente BES creo un entry PDP
-//     insegnamenti.forEach(async insegnamento => {
-//         // console.log(insegnamento)
-//         // let studenti = await get_studenti_bes(idClasse, idStudente);
-//         // console.log(studenti)
-//         // studenti.forEach(async studente => {
-//         console.log(`adding PDP for insegnamento [${insegnamento.id}] - studente [${idStudente}]`);
-//         await add_pdp(insegnamento.idDocente, insegnamento.id, +idStudente, insegnamento.anno);
-//         // })
-//     });
-// }
-
-// main(argv);
-
 
 async function main(argv) {
     if (argv.length != 4) {
