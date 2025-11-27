801f5b761d19aa71dc245078a608e786436e1193
profmancusoa <profmancusoa@gmail.com>
2025-11-27 20:53:23 +0100

    - risolto problema con le date altra relazione nella sezione A del PDP

--- src/routes/pdp/genera_pdp/+page.server.js
+++ src/routes/pdp/genera_pdp/+page.server.js
@@ -375,24 +375,29 @@ export const actions = {
                 // renderer['relazione_altro_data5'] = renderer['relazione_altro_data5'].length != 10 ? '' : renderer['relazione_altro_data5'];
                 // console.log("12f")
 
-                renderer['relazione_ssn_data'] = sanitize(renderer['relazione_ssn_data']);
-                console.log("12a")
-                renderer['relazione_altro_data1'] = sanitize(renderer['relazione_altro_data1']);
-                console.log("12b")
-                renderer['relazione_altro_data2'] = sanitize(renderer['relazione_altro_data2']);
-                console.log("12c")
-                renderer['relazione_altro_data3'] = sanitize(renderer['relazione_altro_data3']);
-                console.log("12d")
-                renderer['relazione_altro_data4'] = sanitize(renderer['relazione_altro_data4']);
-                console.log("12e")
-                renderer['relazione_altro_data5'] = sanitize(renderer['relazione_altro_data5']);
-                console.log("12f")
-
                 renderer['relazione_altro_redattore1'] = sanitize(renderer['relazione_altro_redattore1']);
                 renderer['relazione_altro_redattore2'] = sanitize(renderer['relazione_altro_redattore2']);
                 renderer['relazione_altro_redattore3'] = sanitize(renderer['relazione_altro_redattore3']);
                 renderer['relazione_altro_redattore4'] = sanitize(renderer['relazione_altro_redattore4']);
                 renderer['relazione_altro_redattore5'] = sanitize(renderer['relazione_altro_redattore5']);
+
+                renderer['relazione_ssn_data'] = sanitize(renderer['relazione_ssn_data']);
+
+
+                //uno struno bug è apparso, quindi per mancanza di tempo faccio questo workaround
+                // le date delle relazione_altro è inizialmente ''
+                // se c'e' un redattore allora assegna la data effettiva
+                console.log("12a")                
+                renderer['relazione_altro_data1'] = renderer['relazione_altro_redattore1'].length > 0 ? sanitize(renderer['relazione_altro_data1']) : '';
+                console.log("12b")
+                renderer['relazione_altro_data2'] = renderer['relazione_altro_redattore2'].length > 0 ? sanitize(renderer['relazione_altro_data2']) : '';
+                console.log("12c")
+                renderer['relazione_altro_data3'] = renderer['relazione_altro_redattore3'].length > 0 ? sanitize(renderer['relazione_altro_data3']) : '';
+                console.log("12d")
+                renderer['relazione_altro_data4'] = renderer['relazione_altro_redattore4'].length > 0 ? sanitize(renderer['relazione_altro_data4']) : '';
+                console.log("12e")
+                renderer['relazione_altro_data5'] = renderer['relazione_altro_redattore5'].length > 0 ? sanitize(renderer['relazione_altro_data5']) : '';
+                console.log("12f")
              }
 
              console.log("13")
